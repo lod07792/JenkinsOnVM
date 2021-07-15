6b159a909a1885d641f8bdb6b1dc88e52f9a0d9d
lod0 <lod0@naver.com>
2021-07-15 13:02:49 +0900

    3rd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like lod0@@@@@@@@@@@@@@@!');
+    res.send('I like bts!');
 });
 
 app.get('/start', function(req, res){
