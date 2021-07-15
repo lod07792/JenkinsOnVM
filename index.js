2cf2736d3c462fe15ac671294f3d7b9edc0e4002
lod0 <lod0@naver.com>
2021-07-15 13:32:06 +0900

    7th

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('i like snsd!');
+    res.send('pm2 start');
 });
 
 app.get('/start', function(req, res){
